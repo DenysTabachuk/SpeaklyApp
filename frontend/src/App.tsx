62a2f13ba616442fabcc23cfa06ee0e165ee5ac0
DenysTabachuk <tabakdenuc@gmail.com>
2025-08-15 11:55:21 +0300

    Виніс логіку колекцій з домашньої сторінки в окрему

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Home from "./pages/Home/Home";
+import Collections from "./pages/Collections/Collections";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import Profile from "./pages/Profile/Profile";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <main>
         {/*необхідно розмістити всередині <BrowserRouter>,  щоб він мав доступ до роутінгу (наприклад, для використання Link або useLocation). */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/collections" element={<Collections />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
